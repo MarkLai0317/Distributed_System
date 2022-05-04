33bb7e1fb61c0a350a35c06894e30c7a8e31ba67
Mark Lai <laimark900317@gmail.com>
2022-05-04 22:45:39 +0800

    feat: shardings for load balance
    新增檔案：
    * broker.js -> shards 和 server 之間的broker
    * customer-1.js , customer-2.js -> 處理customer相關的database操作的shard
    * manager-1.js -> 處理 manager相關的database 操作的shard

---
+++ backend/shardings/customer-1.js
@@ -0,0 +1,34 @@
+const customer = require('../services/customer.js');
+
+var mqtt = require('mqtt');
+var client = mqtt.connect('mqtt://localhost:8888', {clientId: 'shardings/customer-1'});
+var option = {
+    qos: 2
+}
+
+client.on('connect', ()=>{
+  console.log("connect success");
+  client.subscribe('sharding/customer-1', option);
+})
+
+// data = {
+//   serviceId: ''  // route
+//   parameters:{},
+//   transactionId:"1234"
+// }
+
+const requestHandler = (serviceId, transactionId,  parameters) =>{
+  
+  let result = customer[serviceId](parameters, transactionId)
+//let result = customer[serviceId](parameters, transactionId)
+  client.publish(transactionId, JSON.stringify(result))
+
+}
+
+client.on('message', (topic, message, packet)=>{
+  let data = JSON.parse(message)
+  console.log(JSON.parse(message))
+  requestHandler(data.serviceId, data.transactionId, data.parameters)
+
+})
+
