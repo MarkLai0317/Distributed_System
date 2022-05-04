4408e979e75dfcd8fb73cf776d9d63d9ecd00dbc
Mark Lai <laimark900317@gmail.com>
2022-05-04 23:11:33 +0800

    feat: 新增 chat 的route for chatting feature
    新增檔案：
    *routes/chat.js -> 正式使用的route
    *routes/tes_chat.js -> 測試api用

---
+++ backend/routes/chat.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+
+//shard(2)
+
+
+module.exports = function(globalVariables){
+
+  const shard = globalVariables.shard 
+
+  
+
+
+  return router
+
+}
