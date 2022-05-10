9fbe48b54f98e4ebd2b3b1eec59adc844da58b7e
YJack0000 <jackleagends@gmail.com>
2022-05-11 01:53:23 +0800

    save message 30% * mongo_op.js -> 可以拿到資料

--- backend/chat_service/mongo_op.js
+++ backend/chat_service/mongo_op.js
@@ -1,1 +1,23 @@
 // @鄭宇傑
+var MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://localhost:27017"
+
+function saveMsg(from, to, msg, date){ //saving message to mongodb 
+    // Connect to the db
+    MongoClient.connect(url, (err, client)=>{
+    
+        if(err) throw err;
+    
+        const db = client.db('dis_sys')
+        db.collection('chat').findOne({_id: from}, (err, result)=>{
+            if(err) throw err;
+
+            console.log(result.msgs.shop01)
+            
+            client.close()
+        })
+
+    });
+}
+
+saveMsg("90", "shop01", "Hello", "12345678")
