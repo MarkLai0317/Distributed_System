9a8428e54d19b98614ec5063a0f8bbb1312a8b06
24135768b <24135768b@gmail.com>
2022-05-11 13:56:11 +0800

    新增可以直接讓後端跑起來的shell script

---
+++ backend/run_backend.sh
@@ -0,0 +1,17 @@
+cd shardings
+node broker.js &
+{
+    sleep 2
+    node customer-1.js 
+} &
+{
+    sleep 2
+    node customer-2.js 
+} &
+{
+    sleep 2
+    node manager-1.js 
+} &
+sleep 4
+cd ..
+node index
