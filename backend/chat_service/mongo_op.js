bae4b288a2f64caaacf8600a7363cfc23f789904
YJack0000 <jackleagends@gmail.com>
2022-05-14 16:05:09 +0800

    mongo_op.js 增增加及刪除訊息功能

--- backend/chat_service/mongo_op.js
+++ backend/chat_service/mongo_op.js
@@ -12,12 +12,56 @@ function saveMsg(from, to, msg, date){ //saving message to mongodb
         db.collection('chat').findOne({_id: from}, (err, result)=>{
             if(err) throw err;
 
-            console.log(result.msgs.shop01)
-            
-            client.close()
+            console.log(result.msgs[to])
+
+            const rec = {
+                Msg: msg,
+                From: from,
+                Date: date
+            }
+
+            result.msgs[to].push(rec)
+            console.log(result.msgs[to])
+
+            db.collection('chat').updateOne({_id: from}, {$set: {msgs: result.msgs}}, (err)=>{
+                if(err) throw err;
+                client.close();
+            })
         })
 
     });
 }
 
-saveMsg("90", "shop01", "Hello", "12345678")
+// saveMsg("91", "shop01", "Hello", "12345678")
+
+function delMsg(from, to, msg, date){ //deleting message from mongodb 
+    // Connect to the db
+    MongoClient.connect(url, (err, client)=>{
+    
+        if(err) throw err;
+    
+        const db = client.db('dis_sys')
+        db.collection('chat').findOne({_id: from}, (err, result)=>{
+            if(err) throw err;
+
+            console.log(result.msgs[to])
+
+            const rec = {
+                Msg: msg,
+                From: from,
+                Date: date
+            }
+
+            result.msgs[to].splice(result.msgs[to].indexOf(rec), 1)
+            console.log(result.msgs[to])
+
+            db.collection('chat').updateOne({_id: from}, {$set: {msgs: result.msgs}}, (err)=>{
+                if(err) throw err;
+                client.close();
+            })
+        })
+
+    });
+}
+
+delMsg("91", "shop01", "Hello", "12345678")
